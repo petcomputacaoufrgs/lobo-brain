f3829003003b53c4e323031a9d02758fddb41af2
Thiago Sotoriva Lermen <49798588+thiagolermen@users.noreply.github.com>
2019-12-19 17:40:56 -0300

    recuperacao das funcoes move.cpp e move_teste.cpp

--- move.cpp
+++ move.cpp
@@ -1,3 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "move.hpp"
+
+using namespace std;
+
+// Seria legal entender isso aqui
+
+//ja sei q to fazendo doidera de ponteiros, vai ser mudado deixa rolar
+/****************************************************************************
+*     tapatanMoves:                                                         *
+*           - Especificacao:                                                *
+*                 - A partir da posicao atual do jogador, gera uma          *
+*                 lista de tuplas (i,j) [(linha, coluna)] dos               *
+*                 possiveis movimentos do jogador.                          *
+*                                                                           *
+*           - Entrada:                                                      *
+*                 - Recebe como parametro a posicao atual do jogador,       *
+*                 ou seja, como o tapatan possui 9 posicoes, a entrada      *
+*                 sera um inteiro [0, 8]                                    *
+*                                                                           *
+*           - Saida:                                                        *
+*                 - Retorna uma lista de tuplas das possiveis posicoes      *
+*                 que o jogador pode ir                                     *
+*                                                                           *
+*           - Observacoes:                                                  *
+*                 - Isso aqui é extremamente ineficiente                    *  
+*                 deve-se implementar algo melhor para tabuleiros maiores   *
+*                 i.e. grafo                                                *
+*****************************************************************************/
 vector<vector<int>> tapatanMoves(int player_pos)
 {
       vector<vector<int>> possible_mov;
@@ -5,7 +37,6 @@ vector<vector<int>> tapatanMoves(int player_pos)
       vector<int> ija;
       vector<int> ijb;
       vector<int> ijc;
-      vector<int> ij;
       vector<int> ijd;
       vector<int> ije;
       vector<int> ijf;
@@ -14,102 +45,68 @@ vector<vector<int>> tapatanMoves(int player_pos)
       switch(player_pos)
       {
       case 0://(0,0)
-         ija[0] = 0; ija[1] = 1;
          ija.push_back(0); ija.push_back(1);
          possible_mov.push_back(ija);
-         ijb[0] = 1; ijb[1] = 0;
 
          ijb.push_back(1); ijb.push_back(0);
          possible_mov.push_back(ijb);
-         ijc[0] = 1; ijc[1] = 1;
 
          ijc.push_back(1); ijc.push_back(1);
          possible_mov.push_back(ijc);
 
          return possible_mov;
       case 1://(0,1)
-         ija[0] = 0; ija[1] = 0;
          ija.push_back(0); ija.push_back(0);
          possible_mov.push_back(ija);
-         ijb[0] = 1; ijb[1] = 1;
 
          ijb.push_back(1); ijb.push_back(1);
          possible_mov.push_back(ijb);
-         ijc[0] = 0; ijc[1] = 2;
-
+         
          ijc.push_back(0); ijc.push_back(2);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
       case 2://(0,2)
-         ij[0] = 0; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 2;
-         possible_mov.push_back(ij);
          ija.push_back(0); ija.push_back(1);
          possible_mov.push_back(ija);
-
+         
          ijb.push_back(1); ijb.push_back(1);
          possible_mov.push_back(ijb);
-
+         
          ijc.push_back(1); ijc.push_back(2);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
       case 3://(1,0)
-         ij[0] = 0; ij[1] = 0;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 2; ij[1] = 0;
-         possible_mov.push_back(ij);
          ija.push_back(0); ija.push_back(0);
          possible_mov.push_back(ija);
 
          ijb.push_back(1); ijb.push_back(1);
          possible_mov.push_back(ijb);
-
+         
          ija.push_back(2); ija.push_back(0);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
       case 4://(1,1)
-         ij[0] = 0; ij[1] = 0;
-         possible_mov.push_back(ij);
-         ij[0] = 0; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 0; ij[1] = 2;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 0;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 2;
-         possible_mov.push_back(ij);
-         ij[0] = 2; ij[1] = 0;
-         possible_mov.push_back(ij);
-         ij[0] = 2; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 2; ij[1] = 2;
-         possible_mov.push_back(ij);
          ija.push_back(0); ija.push_back(0);
          possible_mov.push_back(ija);
-
+         
          ijb.push_back(0); ijb.push_back(1);
          possible_mov.push_back(ijb);
 
          ijc.push_back(0); ijc.push_back(2);
          possible_mov.push_back(ijc);
-
+         
          ijd.push_back(1); ijd.push_back(0);
          possible_mov.push_back(ijd);
 
          ije.push_back(1); ije.push_back(2);
          possible_mov.push_back(ije);
-
+         
          ijf.push_back(2); ijf.push_back(0);
          possible_mov.push_back(ijf);
-
+         
          ijg.push_back(2); ijg.push_back(1);
          possible_mov.push_back(ijg);
 
@@ -117,12 +114,6 @@ vector<vector<int>> tapatanMoves(int player_pos)
          possible_mov.push_back(ijh);
          return possible_mov;
       case 5://(1,2)
-         ij[0] = 0; ij[1] = 2;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 2; ij[1] = 2;
-         possible_mov.push_back(ij);
          ija.push_back(0); ija.push_back(2);
          possible_mov.push_back(ija);
 
@@ -131,58 +122,40 @@ vector<vector<int>> tapatanMoves(int player_pos)
 
          ijc.push_back(2); ijc.push_back(2);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
       case 6://(2,0)
-         ij[0] = 2; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 0;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 1;
-         possible_mov.push_back(ij);
          ija.push_back(2); ija.push_back(1);
          possible_mov.push_back(ija);
-
+         
          ijb.push_back(1); ijb.push_back(0);
          possible_mov.push_back(ijb);
-
+         
          ijc.push_back(1); ijc.push_back(1);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
       case 7://(2,1)
-         ij[0] = 2; ij[1] = 0;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 2; ij[1] = 2;
-         possible_mov.push_back(ij);
          ija.push_back(2); ija.push_back(0);
          possible_mov.push_back(ija);
-
+         
          ijb.push_back(1); ijb.push_back(1);
          possible_mov.push_back(ijb);
-
+         
          ijc.push_back(2); ijc.push_back(2);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
       case 8://(2,2)
-         ij[0] = 2; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 1;
-         possible_mov.push_back(ij);
-         ij[0] = 1; ij[1] = 2;
-         possible_mov.push_back(ij);
          ija.push_back(2); ija.push_back(1);
          possible_mov.push_back(ija);
 
          ijb.push_back(1); ijb.push_back(1);
          possible_mov.push_back(ijb);
-
+         
          ijc.push_back(1); ijc.push_back(2);
          possible_mov.push_back(ijc);
-
+         
          return possible_mov;
    }
 }
