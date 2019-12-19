0b1f0500646d8dc5db461352b552a11ba7d33afc
Thiago <thiago.lermen@gmail.com>
2019-12-19 17:23:13 -0300

    move.cpp atualizada

--- move_teste.cpp
+++ move_teste.cpp
@@ -1,34 +1,22 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include "move.hpp"
-
 using namespace std;
 
 
-void printvet(vector <int> const &vet, int j) {
-   cout << "--> elementos do vetor [" << j << "]: ";
-   
-   for(int j=0; j < vet.size(); j++)
-      cout << vet.at(j) << ' ';
-
-    cout << endl;
-}
-
+// aparentemente as funcoes relacionadas a move.cpp estao funcionando corretamente
 int main()
 {
-    int i = 1;
-    int j = 1;
-    int player_pos = 3*i + j;
+    int player_pos = 0;
+    int player_pos = 4;
 
     vector<vector<int>> possible_mov = tapatanMoves(player_pos);
 
-   // funciona
-   // move = OK
-
-    for(int i = 0; i < possible_mov.size(); i++)
-        printvet(possible_mov.at(i), i);
-
+   // deve-se varrer todo possible_mov, mas como nao sei cpp nao sei fazer isso :D
+    for (int i = 0; i < possible_mov.size(); i++)
+    {
+        for (int j = 0; j < possible_mov[i].size(); j++)
+        {
+            cout << possible_mov[i][j];
+        }
+        cout << endl;
+    }
     return 0;
-}
+} 
